38e7d931392ee2a518e1b4e49022abcbe3db14ef
Caligone <hello@calig.one>
2020-11-05 10:50:39 +0100

    Remove m-share related content

--- src/js/post.js
+++ src/js/post.js
@@ -15,109 +15,16 @@ import {
   makeImagesZoomable
 } from './helpers'
 
-let $aosWrapper = null
-let $progressCircle = null
-let lastScrollingY = window.pageYOffset
-let lastWindowHeight = 0
-let lastDocumentHeight = 0
-let circumference = 0
-let isTicking = false
-
-const onScrolling = () => {
-  lastScrollingY = window.pageYOffset
-  requestTicking()
-}
-
-const adjustShare = (timeout) => {
-  if (!isMobile('1023px')) {
-    $('body').removeClass('share-menu-displayed')
-  } else {
-    $('body').addClass('share-menu-displayed')
-    setTimeout(() => {
-      $aosWrapper.removeAttr('data-aos')
-    }, timeout)
-  }
-}
-
-const onResizing = () => {
-  setHeights()
-  adjustShare(100)
-
-  setTimeout(() => {
-    setCircleStyles()
-    requestTicking()
-  }, 200)
-}
-
-const requestTicking = () => {
-  if (!isTicking) {
-    requestAnimationFrame(updating)
-  }
-
-  isTicking = true
-}
-
-const updating = () => {
-  const progressMax = lastDocumentHeight - lastWindowHeight
-  const percent = Math.ceil((lastScrollingY / progressMax) * 100)
-
-  if (percent <= 100) {
-    setProgress(percent)
-  }
-
-  isTicking = false
-}
-
-const setHeights = () => {
-  lastWindowHeight = window.innerHeight
-  lastDocumentHeight = $(document).height()
-}
-
-const setCircleStyles = () => {
-  const svgWidth = $progressCircle.parent().width();
-  const radiusCircle = svgWidth / 2
-  const borderWidth = isMobile() ? 2 : 3
-
-  $progressCircle.parent().attr('viewBox', `0 0 ${svgWidth} ${svgWidth}`)
-  $progressCircle.attr('stroke-width', borderWidth)
-  $progressCircle.attr('r', radiusCircle - (borderWidth - 1))
-  $progressCircle.attr('cx', radiusCircle)
-  $progressCircle.attr('cy', radiusCircle)
-
-  circumference = radiusCircle * 2 * Math.PI
-
-  $progressCircle[0].style.strokeDasharray = `${circumference} ${circumference}`
-  $progressCircle[0].style.strokeDashoffset = circumference
-}
-
-const setProgress = (percent) => {
-  if (percent <= 100) {
-    const offset = circumference - percent / 100 * circumference
-    $progressCircle[0].style.strokeDashoffset = offset
-  }
-}
-
-const prepareProgressCircle = () => {
-  $progressCircle = $('.js-progress')
-
-  setHeights()
-  setCircleStyles()
-  updating()
-
-  setTimeout(() => {
-    $progressCircle.parent().css('opacity', 1)
-  }, 300)
-}
-
 $(document).ready(() => {
   $aosWrapper = $('.js-aos-wrapper')
   const $scrollButton = $('.js-scrolltop')
+  const $loadComments = $('.js-load-comments')
+  const $commentsIframe = $('.js-comments-iframe')
   const $recommendedSlider = $('.js-recommended-slider')
 
   fitvids('.js-post-content')
 
   adjustImageGallery()
-  adjustShare(1000)
 
   if ($recommendedSlider.length > 0) {
     const recommendedSlider = new Glide('.js-recommended-slider', {
@@ -182,13 +89,11 @@ $(document).ready(() => {
     }, 500)
   })
 
+  $loadComments.click(() => {
+    $loadComments.parent().hide()
+    $commentsIframe.fadeIn('slow')
+  })
+
   managePostImages($)
   makeImagesZoomable($, mediumZoom)
-
-  window.addEventListener('scroll', onScrolling, { passive: true })
-  window.addEventListener('resize', onResizing, { passive: true })
-})
-
-$(window).on('load', () => {
-  prepareProgressCircle()
 })
